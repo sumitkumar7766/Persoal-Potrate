2cb82d04f5dd9e5006e8caa8867e5d728765b4fb
sumitkumar7766 <sumitkumarjaimahakaal@gmail>
2024-12-08 16:21:26 +0530

    8 December Work

---
+++ script.js
@@ -0,0 +1,47 @@
+const openpage1 = () => {
+    window.location.href = 'indexvedio.html';
+}
+
+const openpage2 = () => {
+    window.location.href = 'indexleader.html';
+}
+
+const openpage3 = () => {
+    window.location.href = 'indexchalange.html';
+}
+
+const openpage4 = () => {
+    window.location.href = 'index.html';
+}
+
+const openpage5 = () => {
+    window.location.href = 'indexvedio.html';
+}
+
+const openpage6 = () => {
+    window.location.href = 'indexleader.html';
+}
+
+const openpage7 = () => {
+    window.location.href = 'index.html';
+}
+
+const openpage8 = () => {
+    window.location.href = 'indexvedio.html';
+}
+
+const openpage9 = () => {
+    window.location.href = 'indexchalange.html';
+}
+
+const openpage10 = () => {
+    window.location.href = 'index.html';
+}
+
+const openpage11 = () => {
+    window.location.href = 'indexleader.html';
+}
+
+const openpage12 = () => {
+    window.location.href = 'indexchalange.html';
+}
