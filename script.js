8afa92de3303374a41d9c504fd441d1eef5a066f
Sumit Kumar <sumitkumarjaimahakaal@gmail.com>
2024-12-12 14:17:04 +0530

    12 december

--- script.js
+++ script.js
@@ -45,3 +45,29 @@ const openpage11 = () => {
 const openpage12 = () => {
     window.location.href = 'indexchalange.html';
 }
+
+let collection1 = document.querySelector(".collection1"),
+collection2 = document.querySelector(".collection2");
+
+collection1.addEventListener("click", () => {
+    collection2.hidden = false;
+    collection1.hidden = true;
+    alert("Img collected succesfully");
+})
+
+collection2.addEventListener("click", () => {
+    collection1.hidden = false;
+    collection2.hidden = true;
+    alert("Img Remove From Collection succesfully");
+})
+
+let likedivimg1 = document.querySelector(".likedivimg1"),
+heartlogo = document.querySelector(".heartlogo"),
+liklecount = document.querySelector(".liklecount");
+
+likedivimg1.addEventListener("click", () => {
+    let count = 126;
+    heartlogo.style.color = "red";
+    count ++;
+    liklecount.innerHTML = count;
+})
