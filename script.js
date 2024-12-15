7fc614d196c0fe3d54ebd18d9e428fb0618ceeb7
sumitkumar7766 <sumitkumarjaimahakaal@gmail>
2024-12-16 01:35:27 +0530

    16 December Update

--- script.js
+++ script.js
@@ -52,13 +52,11 @@ collection2 = document.querySelector(".collection2");
 collection1.addEventListener("click", () => {
     collection2.hidden = false;
     collection1.hidden = true;
-    alert("Img collected succesfully");
 })
 
 collection2.addEventListener("click", () => {
     collection1.hidden = false;
     collection2.hidden = true;
-    alert("Img Remove From Collection succesfully");
 })
 
 let likedivimg1 = document.querySelector(".likedivimg1"),
@@ -83,9 +81,16 @@ likedivimg1.addEventListener("dblclick", () => {
 })
 
 let clickimg1 = document.querySelector("#clickimg1"),
+clickimg2 = document.querySelector("#clickimg2"),
+img2div = document.querySelector(".img2div"),
 contanerimgmain = document.querySelector(".contanerimgmain"),
 img1div = document.querySelector(".img1div"),
 back1but = document.querySelector(".back1but");
+
+clickimg2.addEventListener("click", () => {
+    contanerimgmain.hidden = true;
+    img2div.hidden = false;
+})
 
 clickimg1.addEventListener("click", () => {
     contanerimgmain.hidden = true;
@@ -112,3 +117,62 @@ claspdof1.addEventListener("dblclick", () => {
 const freetouse = () => {
     alert("You Can Use this containt to any where Freealy");
 }
+
+const backtomain = () => {
+    contanerimgmain.hidden = false;
+    img1div.hidden = true;
+}
+
+let collect11 = document.querySelector("#collect11"),
+collect12 = document.querySelector("#collect12"),
+follow1 = document.querySelector("#follow1"),
+likecount1 = document.querySelector("#likecount1"),
+heartlogocolor1 = document.querySelector("#heartlogocolor1"),
+likeed1 = document.querySelector("#likeed1"),
+likecountno1 = document.querySelector("#likecountno1");
+
+collect11.addEventListener("click", () => {
+    collect11.hidden = true;
+    collect12.hidden = false;
+})
+
+collect12.addEventListener("click", () => {
+    collect12.hidden = true;
+    collect11.hidden = false;
+})
+
+collect11.addEventListener("dblclick", () => {
+    collect11.hidden = false;
+    collect12.hidden = true;
+})
+
+collect12.addEventListener("dblclick", () => {
+    collect12.hidden = false;
+    collect11.hidden = true;
+})
+
+follow1.addEventListener("click", () => {
+    follow1.innerHTML = 'Following';
+    follow1.style.color = 'green';
+})
+
+follow1.addEventListener("dblclick", () => {
+    follow1.innerHTML = 'Follow';
+    follow1.style.color = 'rgb(99, 99, 99)';
+})
+
+likecount1.addEventListener("click", () => {
+    let count = 232;
+    heartlogocolor1.style.color = "red";
+    count ++;
+    likecountno1.innerHTML = count;
+    likeed1.innerHTML = 'Liked';
+})
+
+likecount1.addEventListener("dblclick", () => {
+    let count = 233;
+    heartlogocolor1.style.color = "rgb(100, 100, 100)";
+    count --;
+    likecountno1.innerHTML = count;
+    likeed1.innerHTML = 'Like';
+})
